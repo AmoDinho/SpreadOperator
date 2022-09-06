81cc4a70d4d688acb5f417cf1f3c7e7626a9fa86
Amo Moloko <u14284783@tuks.co.za>
2022-09-06 07:38:56 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//Copy
+
+const arr = ["a","c","b"]
+
+const arr2 = arr 
+console.log(arr2)
+
+arr2.push("f")
+console.log(arr2)
+
+//copy example 
+
+let arr3 = ["a","c","b"]
+let arr4 = [...arr3]
+console.log(arr4)
+
